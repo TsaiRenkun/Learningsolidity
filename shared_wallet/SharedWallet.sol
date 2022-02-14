983da5bb7f9107d7b72e00217fffb6c8a4ca0d01
Tsairenkun <Tsairenkun@gmail.com>
2022-02-14 17:06:48 +0800

    new

--- shared_wallet/SharedWallet.sol
+++ shared_wallet/SharedWallet.sol
@@ -1,5 +1,20 @@
 pragma solidity >=0.8.0 < 0.9.0;
 
 contract SharedWallet {
-    
+
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "You are not the owner of this contract");
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
+        _to.transfer(_amount);
+    }
+
+    receive() external payable
 }

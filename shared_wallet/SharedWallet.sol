ec6bf7dcdc2b03c44ee25d592382ba66d3c5a369
Tsairenkun <Tsairenkun@gmail.com>
2022-02-14 18:56:52 +0800

    Evetns

--- shared_wallet/SharedWallet.sol
+++ shared_wallet/SharedWallet.sol
@@ -80,9 +80,11 @@ contract Allowance is Ownable {
         return owner() == msg.sender;
     }
 
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
     function setAllownace(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -92,18 +94,26 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount){
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[who] -= _amount;
     }
     
 }
 
 contract SharedWallet is Allowance {
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "contract doesn't have enough money");
         if(!isOwner()){
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
-    receive() external payable;
+    receive() external payable{
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 }
